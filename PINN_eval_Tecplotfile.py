5c73929ecec8e3790557b85b8848525012a95813
Hyungmin.Shin <hyungmin.shin@stud.uni-goettingen.de>
2025-03-05 14:55:40 +0100

    Tecplot modified

--- PINN_eval_Tecplotfile.py
+++ PINN_eval_Tecplotfile.py
@@ -65,22 +65,46 @@ def Derivatives(dynamic_params, all_params, g_batch, model_fns):
     keys = ['u_ref', 'v_ref', 'w_ref', 'u_ref']
 
     all_params["network"]["layers"] = dynamic_params
-    out, out_x = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[0.0, 1.0, 0.0, 0.0]]),(g_batch.shape[0],1)),model_fns)
-    out, out_y = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[0.0, 0.0, 1.0, 0.0]]),(g_batch.shape[0],1)),model_fns)
-    out, out_z = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[0.0, 0.0, 0.0, 1.0]]),(g_batch.shape[0],1)),model_fns)    
-    uvwp = np.concatenate([out[:,k:(k+1)]*all_params["data"][keys[k]] for k in range(len(keys))],1)
-    uvwp[:,-1] = 1.185*uvwp[:,-1]
-    uxs = np.concatenate([out_x[:,k:(k+1)]*all_params["data"][keys[k]]/all_params["domain"]["in_max"][0,1] for k in range(len(keys))],1)
-    uys = np.concatenate([out_y[:,k:(k+1)]*all_params["data"][keys[k]]/all_params["domain"]["in_max"][0,2] for k in range(len(keys))],1)
-    uzs = np.concatenate([out_z[:,k:(k+1)]*all_params["data"][keys[k]]/all_params["domain"]["in_max"][0,3] for k in range(len(keys))],1)
-    deriv_mat = np.concatenate([np.expand_dims(uxs,2),np.expand_dims(uys,2),np.expand_dims(uzs,2)],2)
-    vor_mag = np.sqrt((deriv_mat[:,1,2]-deriv_mat[:,2,1])**2+
-                      (deriv_mat[:,2,0]-deriv_mat[:,0,2])**2+
-                      (deriv_mat[:,0,1]-deriv_mat[:,1,0])**2)
-    Q = 0.5 * sum(-np.abs(0.5 * (deriv_mat[:, i, j] + deriv_mat[:, j, i]))**2 +
-                  np.abs(0.5 * (deriv_mat[:, i, j] - deriv_mat[:, j, i]))**2 
-                  for i in range(3) for j in range(3))
-    return uvwp, vor_mag, Q
+    out, out_t = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[1.0, 0.0, 0.0, 0.0]]),(g_batch.shape[0],1)),model_fns)
+    _, out_x = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[0.0, 1.0, 0.0, 0.0]]),(g_batch.shape[0],1)),model_fns)
+    _, out_y = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[0.0, 0.0, 1.0, 0.0]]),(g_batch.shape[0],1)),model_fns)
+    _, out_z = equ_func2(all_params, g_batch, jnp.tile(jnp.array([[0.0, 0.0, 0.0, 1.0]]),(g_batch.shape[0],1)),model_fns)    
+
+    u = all_params["data"]['u_ref']*out[:,0:1]
+    v = all_params["data"]['v_ref']*out[:,1:2]
+    w = all_params["data"]['w_ref']*out[:,2:3]
+
+    ut = all_params["data"]['u_ref']*out_t[:,0:1]/all_params["data"]["domain_range"]["t"][1]
+    vt = all_params["data"]['v_ref']*out_t[:,1:2]/all_params["data"]["domain_range"]["t"][1]
+    wt = all_params["data"]['w_ref']*out_t[:,2:3]/all_params["data"]["domain_range"]["t"][1]
+
+    ux = all_params["data"]['u_ref']*out_x[:,0:1]/all_params["data"]["domain_range"]["x"][1]
+    vx = all_params["data"]['v_ref']*out_x[:,1:2]/all_params["data"]["domain_range"]["x"][1]
+    wx = all_params["data"]['w_ref']*out_x[:,2:3]/all_params["data"]["domain_range"]["x"][1]
+
+    uy = all_params["data"]['u_ref']*out_y[:,0:1]/all_params["data"]["domain_range"]["y"][1]
+    vy = all_params["data"]['v_ref']*out_y[:,1:2]/all_params["data"]["domain_range"]["y"][1]
+    wy = all_params["data"]['w_ref']*out_y[:,2:3]/all_params["data"]["domain_range"]["y"][1]
+
+    uz = all_params["data"]['u_ref']*out_z[:,0:1]/all_params["data"]["domain_range"]["z"][1]
+    vz = all_params["data"]['v_ref']*out_z[:,1:2]/all_params["data"]["domain_range"]["z"][1]
+    wz = all_params["data"]['w_ref']*out_z[:,2:3]/all_params["data"]["domain_range"]["z"][1]
+    
+    acc_x = ut + u*ux + v*uy + w*uz
+    acc_y = vt + u*vx + v*vy + w*vz
+    acc_z = wt + u*wx + v*wy + w*wz
+    acc = np.concatenate([acc_x, acc_y, acc_z],1)
+
+    matrix1 = [ux, uy, uz, vx, vy, vz, wx, wy, wz]
+    matrix2 = [ux, vx, wx, uy, vy, wy, uz, vz, wz]
+    vor_mag = np.sqrt((uz-wx)**2+(uy-vx)**2+(vz-wy)**2)
+    Q = 0
+    for i,j in zip(matrix1, matrix2):
+        S = 0.5*(i + j)
+        P = 0.5*(i - j)
+        Q = Q + 0.5* ((np.abs(P))**2 - (np.abs(S))**2)
+
+    return uvwp, acc, vor_mag, Q
 
 #%%
 if __name__ == "__main__":
@@ -153,10 +177,11 @@ if __name__ == "__main__":
 
 #%%
         dynamic_params = all_params["network"].pop("layers")
-        uvwp, vor_mag, Q = zip(*[Derivatives(dynamic_params, all_params, eval_grid[i:i+10000], model_fn) 
+        uvwp, acc, vor_mag, Q = zip(*[Derivatives(dynamic_params, all_params, eval_grid[i:i+10000], model_fn) 
                                 for i in range(0, eval_grid.shape[0], 10000)])
         uvwp = np.concatenate(uvwp, axis=0)
         vor_mag = np.concatenate(vor_mag, axis=0)
+        acc = np.concatenate(acc, axis=0)
         Q = np.concatenate(Q, axis=0)
 
         filename = "Tecplot_data/"+checkpoint_fol+"/QUD_eval_"+str(timestep)+".dat"
@@ -167,6 +192,8 @@ if __name__ == "__main__":
         X, Y, Z = (x_e[0,0,:].shape[0], y_e[0,:,0].shape[0], z_e[:,0,0].shape[0])
         vars = [('u_pred[m/s]',np.float32(uvwp[:,0].reshape(-1))), ('v_pred[m/s]',uvwp[:,1].reshape(-1)),
                 ('w_pred[m/s]',uvwp[:,2].reshape(-1)), ('p_pred[Pa]',uvwp[:,3].reshape(-1)),
+                ('acc_x [m/s^2]',acc[:,0].reshape(-1)), ('acc_y [m/s^2]',acc[:,1].reshape(-1)),
+                ('acc_z [m/s^2]',acc[:,2].reshape(-1)),
                 ('vormag[1/s]',vor_mag.reshape(-1)), ('Q[1/s^2]', Q.reshape(-1))]
         fw = 27
         tecplot_Mesh(filename, X, Y, Z, x_e.reshape(-1), y_e.reshape(-1), z_e.reshape(-1), vars, fw)
